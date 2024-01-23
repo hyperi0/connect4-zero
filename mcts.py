fixed negative signs in mcts and fixed crossentropy loss, agent actually works now

mcts.py
@@ -18,7 +18,7 @@ class MCTS():
     
     def search(self, s):
         if connectx.is_terminal_grid(s, self.config):
-            return connectx.score_game(s, self.config)
+            return -connectx.score_game(s, self.config)
         
         # first visit: initialize to neural net's predicted action probs and value
         if s not in self.visited:
@@ -28,7 +28,7 @@ class MCTS():
             self.P[s] = [p * mask for p, mask in zip(pi, legal_mask)]
             self.N[s] = [0 for _ in range(self.config.columns)]
             self.Q[s] = [0 for _ in range(self.config.columns)]
-            return v
+            return -v
         
         # choose action with best upper confidence bound
         max_u, best_a = -np.inf, -1

nnet_torch.py
@@ -10,7 +10,6 @@ class PolicyNet(nn.Module):
         self.conv2 = nn.Conv2d(32, 32, 3)
         self.fc = nn.Linear(32 * (columns - 4) * (rows - 4), 64)
         self.pi_fc = nn.Linear(64, columns)
-        self.pi_sm = nn.Softmax(dim=1)
         self.v = nn.Linear(64, 1)
     
     def forward(self, x):
@@ -18,8 +17,8 @@ class PolicyNet(nn.Module):
         x = F.relu(self.conv2(x))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc(x))
-        pi = self.pi_sm(self.pi_fc(x))
-        v = self.v(x)
+        pi = F.log_softmax(self.pi_fc(x), dim=1)
+        v = torch.tanh(self.v(x))
         return pi, v
     
 class ExperienceDataset(Dataset):
@@ -39,23 +38,27 @@ class Policy():
     def __init__(self, device='cpu'):
         self.device = device
         self.nnet = PolicyNet().to(device)
-        
+    
+    def loss_fn_pi(self, pi_target, pi_pred):
+        return -torch.sum(pi_target * pi_pred) / pi_target.size()[0]
+    
     def train(self, examples, batch_size=64, epochs=5, lr=1e-2):
         train_dataset = ExperienceDataset(examples, device=self.device)
         train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
         optimizer = torch.optim.Adam(self.nnet.parameters(), lr=lr)
-        loss_fn_pi = torch.nn.CrossEntropyLoss()
         loss_fn_v = torch.nn.MSELoss()
         for i in range(epochs):
             for batch in train_dataloader:
+                optimizer.zero_grad()
                 s, pi, v = batch['s'], batch['pi'], batch['v']
                 pi_pred, v_pred = self.nnet(s)
-                loss = loss_fn_pi(pi_pred, pi) + loss_fn_v(v_pred.squeeze(), v)
-                optimizer.zero_grad()
+                loss = self.loss_fn_pi(pi, pi_pred) + loss_fn_v(v_pred.squeeze(), v)
                 loss.backward()
                 optimizer.step()
 
     def predict(self, s):
         input = torch.tensor(s, dtype=torch.float).reshape((1,1,6,7)).to(self.device)
-        pi, v = self.nnet(input)
-        return pi[0].tolist(), v[0].item()
+        self.nnet.eval()
+        with torch.no_grad():
+            pi, v = self.nnet(input)
+            return torch.exp(pi)[0].tolist(), v[0].item()